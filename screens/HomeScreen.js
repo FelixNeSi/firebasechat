cc2bd09fba14cd8b728731fd6d0f45912bed330d
FelixNeSi <felixneedhamsimpson@gmail.com>
2019-05-21 16:24:16 +0100

    chat between clients
    fixed chatting between things 32 31

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,6 +4,7 @@ import User from '../User';
 import firebase from 'firebase';
 import styles from '../constants/styles';
 
+
 export default class HomeScreen extends React.Component {
     static navigationOptions = {
         title: 'Chats'
@@ -18,11 +19,15 @@ export default class HomeScreen extends React.Component {
         dbRef.on('child_added', (val) => {
             let person = val.val();
             person.phone = val.key;
-            this.setState((prevState) => {
-                return {
-                    users: [...prevState.users, person]
-                }
-            })
+            if (person.phone === User.phone) {
+                User.name = person.name
+            } else {
+                this.setState((prevState) => {
+                    return {
+                        users: [...prevState.users, person]
+                    }
+                })
+            }
         })
     }
     _logout = async () => {
@@ -32,10 +37,10 @@ export default class HomeScreen extends React.Component {
 
     renderRow = ({ item }) => {
         return (
-            <TouchableOpacity 
+            <TouchableOpacity
                 onPress={() => this.props.navigation.navigate('Chat', item)}
-                style={{padding:10, borderBottomColor:'#ccc', borderWidth:1}}>
-                <Text style={{fontSize:20}}>{item.name}</Text>
+                style={{ padding: 10, borderBottomColor: '#ccc', borderWidth: 1 }}>
+                <Text style={{ fontSize: 20 }}>{item.name}</Text>
             </TouchableOpacity>
         )
     }
