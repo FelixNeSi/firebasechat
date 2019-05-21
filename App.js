4694e267fca8ec5cc189a5ca82fd7e1ab82489f7
FelixNeSi <felixneedhamsimpson@gmail.com>
2019-05-21 10:27:11 +0100

    Adding storage
    use async storage to sotre a user's phone number if they enter the correct number

--- App.js
+++ App.js
@@ -1,19 +1,56 @@
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {StyleSheet, Text, Alert, AsyncStorage, TouchableOpacity, TextInput, View} from 'react-native';
 
 export default class App extends Component {
+  state = {
+    phone: '',
+    name: ''
+  }
+
+  handleChange = key => val => {
+    this.setState({ [key]: val})
+  }
+
+  componentWillMount(){
+    AsyncStorage.getItem('userPhone').then(val=>{
+      if(val){
+        this.setState({phone: val})
+      }
+    })
+  }
+
+  submitForm = async () => {
+    if(this.state.phone.length < 10){
+      Alert.alert('Error', 'Wrong phone number')
+    }else if (this.state.name.length < 3){
+      Alert.alert('Error', 'Wrong name')
+    }else{
+      //save user data
+      await AsyncStorage.setItem('userPhone',this.state.phone)
+    }
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
         <TextInput 
           placeholder="Phone number"
+          keyboardType="number-pad"
           style={styles.input}
+          value={this.state.phone}
+          onChangeText={this.handleChange('phone')}
         />
         <TextInput 
           placeholder="Name"
           style={styles.input}
+          value={this.state.name}
+          onChangeText={this.handleChange('name')}
         />
+        <TouchableOpacity onPress={this.submitForm}>
+          <Text style={styles.btnText}>Enter</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -30,6 +67,12 @@ const styles = StyleSheet.create({
     padding: 10,
     borderWidth:1,
     borderColor:'#ccc',
-    width:'90%'
+    width:'90%',
+    marginBottom:10,
+    borderRadius:5
+  },
+  btnText: {
+    color:'darkblue',
+    fontSize:20
   }
 });
