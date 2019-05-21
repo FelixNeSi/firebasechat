3d35eed6c0f67c7ab5efbe9d20cbaa802d729b89
FelixNeSi <felixneedhamsimpson@gmail.com>
2019-05-21 14:27:22 +0100

    chat improvement
    added improvements to chat ui and such

--- constants/styles.js
+++ constants/styles.js
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
         padding: 10,
         borderWidth: 1,
         borderColor: '#ccc',
-        width: '90%',
+        width: '80%',
         marginBottom: 10,
         borderRadius: 5
     },
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { SafeAreaView, Text, TextInput, TouchableOpacity } from 'react-native';
-import styles from '../constants/styles'
+import { SafeAreaView, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import styles from '../constants/styles';
+import User from '../User';
+import firebase from 'firebase';
+
 
 export default class ChatScreen extends React.Component {
     static navigationOptions = ({ navigation }) => {
@@ -8,9 +11,15 @@ export default class ChatScreen extends React.Component {
             title: navigation.getParam('name', null)
         }
     }
-
-    state = {
-        textMessage: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            person: {
+                name: props.navigation.getParam('name'),
+                phone: props.navigation.getParam('phone'),
+            },
+            textMessage: ''
+        }
     }
 
     handleChange = key => val => {
@@ -18,21 +27,36 @@ export default class ChatScreen extends React.Component {
     }
 
     sendMessage = async () => {
-        
+        if (this.state.textMessage.length > 0) {
+            let msgId = firebase.database().ref('messages').child(User.phone).child(this.state.person.phone).push().key;
+            let updates = {};
+            let message = {
+                message: this.state.textMessage,
+                time: firebase.database.ServerValue.TIMESTAMP,
+                from: User.phone
+            }
+            updates['message/' + User.phone + '/' + this.state.person.phone + '/' + msgId] = message;
+            updates['message/' + this.state.person.phone + User.phone + '/' + '/' + msgId] = message;
+            firebase.database().ref().update(updates);
+            this.setState({ textMessage: '' });
+        }
     }
 
 
     render() {
         return (
             <SafeAreaView>
-                <TextInput
-                    style={styles.input}
-                    value={this.state.textMessage}
-                    onChangeText={this.handleChange('textMessage')}
-                />
-                <TouchableOpacity onPress={this.sendMessage}>
-                    <Text style={styles.btnText}>Send</Text>
-                </TouchableOpacity>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <TextInput
+                        style={styles.input}
+                        value={this.state.textMessage}
+                        placeholder="Type a message..."
+                        onChangeText={this.handleChange('textMessage')}
+                    />
+                    <TouchableOpacity onPress={this.sendMessage}>
+                        <Text style={styles.btnText}>Send</Text>
+                    </TouchableOpacity>
+                </View>
             </SafeAreaView>
         )
     }
