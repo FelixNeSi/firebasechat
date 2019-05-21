7275a8ca12b8ecd313a5b3de2a2657fbfb36d032
FelixNeSi <felixneedhamsimpson@gmail.com>
2019-05-21 17:42:01 +0100

    input box added to profile page
    added an input box to the profile page

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import { SafeAreaView, Text, TextInput } from 'react-native';
 import User from '../User';
 import styles from '../constants/styles';
 
 export default class ProfileScreen extends React.Component {
     static navigationOptions = {
-        title:'Profile'
+        title: 'Profile'
     }
-    render(){
-        return(
+
+    state = {
+        name: User.name
+    }
+
+    handleChange = key => val => {
+        this.setState({ [key]: val })
+    }
+
+    render() {
+        return (
             <SafeAreaView style={styles.container}>
-                <Text style={{fontSize:20}}>
+                <Text style={{ fontSize: 20 }}>
                     {User.phone}
                 </Text>
-                <Text style={{fontSize:20}}>
-                {User.name}
+                <Text style={{ fontSize: 20 }}>
+                    {User.name}
                 </Text>
+                <TextInput
+                    style={styles.input}
+                    value={this.state.name}
+                    onChangeText={this.handleChange}
+                />
             </SafeAreaView>
         )
     }
